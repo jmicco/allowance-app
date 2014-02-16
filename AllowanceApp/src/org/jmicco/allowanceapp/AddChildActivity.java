358013be5097761ffe29ed95221629d622896816
jmicco <john.micco@gmail.com>
2014-02-16 10:25:53 -0800

    Remove the child project and build the V1 project

--- AllowanceApp/src/org/jmicco/allowanceapp/AddChildActivity.java
+++ AllowanceApp/src/org/jmicco/allowanceapp/AddChildActivity.java
@@ -5,8 +5,6 @@ import java.util.Locale;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -53,22 +51,5 @@ public class AddChildActivity extends Activity {
 	protected void onDestroy() {
 		super.onDestroy();
 		repository.close();
-	}
-    
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }

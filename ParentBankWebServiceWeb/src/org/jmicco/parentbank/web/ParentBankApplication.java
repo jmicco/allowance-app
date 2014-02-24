f50d23946468f89fa4e2189347159c2bbe97516d
jmicco <john.micco@gmail.com>
2014-02-23 20:57:05 -0800

    Test out creating a simple REST web service in glassfish server

---
+++ ParentBankWebServiceWeb/src/org/jmicco/parentbank/web/ParentBankApplication.java
@@ -0,0 +1,19 @@
+package org.jmicco.parentbank.web;
+
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ParentBankApplication extends Application {
+
+	@Override
+	public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
+        resources.add(TestUri.class);
+        return resources;
+	}
+
+	
+}
---
+++ ParentBankWebServiceWeb/src/org/jmicco/parentbank/web/TestUri.java
@@ -0,0 +1,15 @@
+package org.jmicco.parentbank.web;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/testuri")
+public class TestUri {
+
+	@GET
+	@Produces("text/plain")
+	public String getMessage() {
+		return "Hello, World";
+	}
+}
